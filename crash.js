6bfa77a63228c7b75aa8e9de984d7a4fc3f3b3e2
Trevor Morris <trevor.morris@machinemetrics.com>
2017-07-13 16:35:30 -0400

    initial rev

---
+++ crash.js
@@ -0,0 +1,68 @@
+#!/usr/bin/env node
+
+'use strict';
+
+/* eslint-disable no-useless-computed-key */
+
+try {
+  crashEntry();
+} catch (e) {
+  console.log(e);
+  process.exit(0);
+}
+console.log('no crash');
+process.exit(1);
+
+function crashEntry() {
+  weirdBootstrap();
+  const entities = getEntities();
+
+  for (let i = 0; i < 10; i += 1) {
+    console.error('calling csvRows iteration:', i);
+    csvRows(entities);
+  }
+}
+
+function csvRows(entities) {
+  for (let i = 0; i < 10; i += 1) {
+    entityIntervalFn(entities[0], i);
+    entityIntervalFn(entities[1], i);
+  }
+}
+
+function entityIntervalFn(entity, i) {
+  const iData = entity.intervalData[i];
+  const val1 = iData ? iData.key1 : null;
+
+  const result = {
+    ['cruft']: 'foo',
+    val1,
+  };
+
+  console.log({ i, iData });
+
+  result.key3 = 1;
+}
+
+function getEntities() {
+  return [{
+    intervalData: [
+      null,
+      { key1: 1,
+        key2: undefined },
+    ]
+  }, {
+    intervalData: [
+      null,
+      { key1: 1,
+        key2: undefined },
+    ]
+  }];
+}
+
+function weirdBootstrap() {
+  return {
+    key1: 0.7,
+    key2: undefined,
+  };
+}
